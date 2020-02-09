4b865d852373d2d8331227502a5ebb7bd551688e
DwayneL <dr2@ameritech.net>
2020-02-09 16:31:05 -0600

    prettied up app.js file

--- app.js
+++ app.js
@@ -12,73 +12,66 @@ var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(express.json());
 
-// DATA 
+// DATA
 // ===============================================================
 
-
-
 // ROUTES
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-})
-
-app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "./public/notes.html")); 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/api/notes", function (req,res){
-    res.send(dataJSON)     
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.post("/api/notes", function(req,res){
-    const addNote = {
-        id: dataJSON.length +1,
-        title: req.body.title,
-        text: req.body.text,
-    };
-
-
-    dataJSON.push(addNote)
-    res.json(dataJSON)
-    console.log(dataJSON)
-
-    fs.writeFile("./db/db.json",JSON.stringify(dataJSON), (err)=> {
-        if (err) throw err;
-        console.log("post works")
-    })
+app.get("/api/notes", function(req, res) {
+  res.send(dataJSON);
 });
 
-app.delete("/api/notes/:id", function(req,res){
-    // const deleteId = req.params.id;
+app.post("/api/notes", function(req, res) {
+  const addNote = {
+    id: dataJSON.length + 1,
+    title: req.body.title,
+    text: req.body.text
+  };
+
+  dataJSON.push(addNote);
+  res.json(dataJSON);
+  console.log(dataJSON);
+
+  fs.writeFile("./db/db.json", JSON.stringify(dataJSON), err => {
+    if (err) throw err;
+    console.log("post works");
+  });
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  // const deleteId = req.params.id;
+  // console.log(deleteId);
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    const deleteId = parseInt(req.params.id);
+    parseData = JSON.parse(data);
     // console.log(deleteId);
-    fs.readFile("./db/db.json", (err, data) =>{
-        if (err) throw (err);
-        const deleteId = parseInt(req.params.id);
-        parseData = JSON.parse(data);
-        // console.log(deleteId);
-        for (i=0; i<parseData.length; i++) {
-            // console.log(parseData[i].id)
+    for (i = 0; i < parseData.length; i++) {
+      // console.log(parseData[i].id)
 
-            if (deleteId === parseData[i].id) {
-                console.log(parseData[i].id)
-                dataJSON.splice(i,1)
-            }
-        }
-    })
-    fs.writeFile("./db/db.json",JSON.stringify(dataJSON), (err)=> {
-        if (err) throw err;
-       
-    })
+      if (deleteId === parseData[i].id) {
+        console.log(parseData[i].id);
+        dataJSON.splice(i, 1);
+      }
+    }
+  });
+  fs.writeFile("./db/db.json", JSON.stringify(dataJSON), err => {
+    if (err) throw err;
+  });
 
-    res.send(dataJSON)
-    
+  res.send(dataJSON);
+});
 
-})
-
-
-app.listen(PORT, function(){
-    console.log("App is listening on port" + PORT);
-})
+app.listen(PORT, function() {
+  console.log("App is listening on port" + PORT);
+});
