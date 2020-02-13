7282fd0a538eb07c489e49de19416413e436db9c
DwayneL <dr2@ameritech.net>
2020-02-12 19:57:46 -0600

    deleted port 3000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var dataJSON = require("./db/db.json");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
